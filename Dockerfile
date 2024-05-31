5dcfc6d58849f25d03f538184a3c9abf79b88143
Monkey Widget <monkeywidget@users.noreply.github.com>
2024-05-30 18:47:04 -0700

    working PHP / perl Docker

--- Dockerfile
+++ Dockerfile
@@ -9,7 +9,7 @@ RUN apt-get update && \
 RUN rm /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
 # Copy the new site configuration template
-COPY cmacc.template.conf /etc/nginx/sites-available/cmacc.template.conf
+COPY cmacc.template.conf /etc/nginx/cmacc.template.conf
 
 # Copy the service start script
 COPY start-services.sh /usr/local/bin/start-services.sh
@@ -22,6 +22,7 @@ RUN rm -rf /var/www/html && mkdir -p /var/www/html
 COPY vendor /var/www/html/vendor
 COPY Doc /var/www/html/Doc
 COPY File /var/www/html/File
+COPY image /var/www/html/image
 COPY png /var/www/html/png
 
 # Set permissions
@@ -30,6 +31,9 @@ RUN chown -R www-data:www-data /var/www/html
 # Copy PHP files
 COPY index.php /var/www/html/index.php
 COPY i.php /var/www/html/i.php
+
+# debugging utilities
+RUN apt-get install -y iproute2 lsof netcat-openbsd socat
 
 # Expose port
 EXPOSE 80
--- start-services.sh
+++ start-services.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Ensure the /run/php directory exists and is owned by www-data
+mkdir -p /run/php
+chown -R www-data:www-data /run/php
+
 # Find the installed PHP-FPM service
 PHP_FPM_SERVICE=$(ls /etc/init.d/ | grep php | grep -E 'fpm$')
 PHP_FPM_VERSION=$(echo $PHP_FPM_SERVICE | sed 's/php//g' | sed 's/-fpm//g')
@@ -9,7 +13,7 @@ echo "PHP-FPM version: $PHP_FPM_VERSION"
 
 # Substitute the PHP-FPM version in the Nginx config template
 export PHP_FPM_VERSION
-envsubst '${PHP_FPM_VERSION}' < /etc/nginx/sites-available/cmacc.template.conf > /etc/nginx/sites-available/cmacc.conf
+envsubst '${PHP_FPM_VERSION}' < /etc/nginx/cmacc.template.conf > /etc/nginx/sites-available/cmacc.conf
 ln -s /etc/nginx/sites-available/cmacc.conf /etc/nginx/sites-enabled/cmacc.conf
 
 # Start PHP-FPM service
@@ -19,6 +23,9 @@ else
     echo "PHP-FPM service not found"
     exit 1
 fi
+
+# add a test file
+echo "<?php phpinfo(); ?>" > /var/www/html/test.php
 
 # Start Nginx
 nginx -g 'daemon off;'
